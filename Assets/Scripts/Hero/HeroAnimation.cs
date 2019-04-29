0bf126d05e76a3421be010fec868c846da08a2f2
Xir <prssergey@gmail.com>
2019-04-29 15:33:06 +0300

    fix: Слой под стены,анимация бега,скрипт переключатель анимации

---
+++ Assets/Scripts/Hero/HeroAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroAnimation : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        if (h!=0 && v==0)
+            anim.SetBool("IsRun", true);
+        else
+            anim.SetBool("IsRun", false);
+    }
+}
